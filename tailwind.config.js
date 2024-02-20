55fd51fce376ac021246f83af8b1c91e049fae23
Om Yadav <omydv3@gmail.com>
2024-02-20 22:05:16 +0530

    template 3 done

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ export default {
       colors: {
         red: "#ab0f1b",
         blue: "#3f51b5",
-        "blue-200" : "#3f51b5"
+        "blue-200" : "#3f51b5",
+        "t3-blue": "#538bbb",
+        "t3-grey": "#707185",
+      },
+      backgroundImage: {
+        't3-bgImage': "url('/resume-3-bg.webp')"
       }
     },
   },
